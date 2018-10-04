76ed01c79f2bdc86348b4e9c1b3d75fee082d30b
Vasiliy I Ozerov <vasiliyozerov@gmail.com>
2018-10-04 20:19:12 +0300

    Switched from strings to symbols

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  fmt.Fprintf(w, "The sum of %d and %d equal %d!", a, b, sum(a,b))
+  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,b))
 }
 
 func main() {
