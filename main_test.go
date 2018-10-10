d7a6c3398974f93a33178551ee315cabbdad2f37
Ilya Glotov <iglotov@gmail.com>
2018-10-11 00:11:09 +0700

    test fixed

--- main_test.go
+++ main_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    total := sum(5, 5)
-    if total != 10 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    total := sum(5, 51)
+    if total != 56 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 56)
     }
 }
 
