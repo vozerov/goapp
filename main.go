595838214c609536d652e79df466845f720829a7
Vasiliy I Ozerov <vasiliyozerov@gmail.com>
2018-10-11 16:55:47 +0300

    Switched to json

--- main.go
+++ main.go
@@ -1,37 +1,60 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
 	"strconv"
+	"encoding/json"
 )
 
+type Reply struct {
+  Status string
+  Code int
+  Result int
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+  var a,b int
+  var err error
+
+  response := Reply{Status: "OK", Code: 0, Result: 0}
+
   keys, ok := r.URL.Query()["a"]
   if !ok || len(keys[0]) < 1 {
-      log.Println("Url Param 'a' is missing")
-      return
-  }
-
-  a, err := strconv.Atoi(keys[0])
-
-  if err != nil {
-    return
+    response.Status = "param A is missing"
+	response.Code = 1
+  } else {
+    a, err = strconv.Atoi(keys[0])
+    if err != nil {
+      response.Status = "param A is not a number"
+	  response.Code = 2
+    }
   }
 
   keys, ok = r.URL.Query()["b"]
   if !ok || len(keys[0]) < 1 {
-      log.Println("Url Param 'b' is missing")
-      return
-  }
-  b, err := strconv.Atoi(keys[0])
-  if err != nil {
-    log.Println("Bad number!")
-    return
+    response.Status = "param B is missing"
+	response.Code = 3
+  } else {
+    b, err = strconv.Atoi(keys[0])
+    if err != nil {
+      response.Status = "param B is not a number"
+	  response.Code = 4
+    }
   }
 
-  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,a))
+  if response.Code == 0 {
+    response.Result = sum(a,b)
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+
+  data, err := json.Marshal(response)
+  if err != nil {
+    log.Println("ERROR: Can't marshal data")
+  }
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(data)
 }
 
 func main() {
