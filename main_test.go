13ec9491fb9bc6450c8e92c476392a499cdcc498
Pavel Kopalov <hitman959@gmail.com>
2018-10-12 17:32:58 +0300

    Sum bag fix

--- main_test.go
+++ main_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    total := sum(5, 5)
-    if total != 10 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+    total := sum(5, 6)
+    if total != 11 {
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 11)
     }
 }
 
