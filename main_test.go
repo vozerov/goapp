ff47375e81555491825411a674c61e1ce22189d0
Филиппов Валерий <liquidpredator@gmail.com>
2018-10-14 21:18:30 +0300

    Fix main_test.go

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSum(t *testing.T) {
     total := sum(5, 6)
     if total != 11 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 11)
     }
 }
 
