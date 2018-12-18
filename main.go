e3d39a197594164221d091d05e9ee589b9bc9e68
Vasiliy I Ozerov <vasiliyozerov@gmail.com>
2018-12-18 19:44:23 +0300

    Fixed summing error

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,a))
+  fmt.Fprintf(w, "%d + %d = %d", a, b, sum(a,b))
 }
 
 func main() {
